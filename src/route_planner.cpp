85f70d5ea459b9a50544747f454b650d090d1580
Brigitta Jesica Kartono <brigitta-jesica.kartono@bmw.de>
2021-11-14 20:56:25 +0100

    commit

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -10,7 +10,8 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-
+    *RoutePlanner::start_node = model.FindClosestNode(start_x, start_y); //RoutePlanner::startnode is a pointer
+    *RoutePlanner::end_node = model.FindClosestNode(end_x, end_y); //RoutePlanner::endnode is a pointer
 }
 
 
@@ -20,7 +21,13 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // - Node objects have a distance method to determine the distance to another node.
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
+    RouteModel::Node end_node;
+    end_node = *RoutePlanner::end_node;
+    
+    float h_value;
+    h_value = *node->distance(end_node);
 
+    return h_value;
 }
 
 
